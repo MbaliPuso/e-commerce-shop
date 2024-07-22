918188543bdf6e75a5aab4c3fc942ffaf40599a2
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-22 02:12:08 +0200

    payment page styling

--- src/pages/AddPayment.jsx
+++ src/pages/AddPayment.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import '../styles/AddPayment.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreditCard } from "@fortawesome/free-solid-svg-icons";
 
 const AddPayment = () => {
     const [formData, setFormData] = useState({
@@ -29,41 +32,50 @@ const AddPayment = () => {
                     <div className="select-card-heading">
                         SELECT A CARD
                     </div>
-                    
+                    <div className="card-list">
+                        <ul>
+                            <li className='card details'></li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="container add-card">
                     <div className="new-card-heading">
                         ADD NEW CARD
                     </div>
                     <form className='add-card-form' onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Cardholder Name</label>
+                        <div className="form-group cardholder">
+                            <label htmlFor="name" className='label-name'>Cardholder Name</label>
                             <input type="text" name='cardHolderName' placeholder='John Maker' value={formData.cardHolderName} onChange={handleChange} className='form-control' required />
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="number">Card Number</label>
+                        <div className="form-group cardnumber">
+                            <label htmlFor="number" className='label-name'>Card Number</label>
                             <input type="number" name='cardNumber' placeholder='5126-5987-2214-7621' value={formData.cardNumber} onChange={handleChange} className='form-control' required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group expiry-cvc">
                             <div className="row">
                                 <div className="col-4">
-                                    <label htmlFor="date">Expiry Date</label>
+                                    <label htmlFor="date" className='label-name'>Expiry Date</label>
                                     <input type="text" name='expiryDate' placeholder='MM/YYYY' value={formData.expiryDate} onChange={handleChange} className='form-control' required />
                                 </div>
                                 <div className="col-3">
-                                <label htmlFor="cvc">CVC</label>
+                                <label htmlFor="cvc" className='label-name'>CVC</label>
                                 <input type="number" name='cvc' placeholder='123' value={formData.cvc} onChange={handleChange} className='form-control' required />
                                 </div>
                             </div>
                         </div>
-                        <div className="form-check">
-                            <input type="checkbox" name="defaultPayment" value={formData.defaultPayment} onChange={handleChange} className='form-check-input' />
-                            <label htmlFor="defaultPayment">Save this as your default payment method</label>
+                        <div className="form-check default-payment">
+                            <input type="checkbox" name="defaultPayment" value={formData.defaultPayment} onChange={handleChange} className='form-check-input custom-checkbox' />
+                            <label htmlFor="defaultPayment" className='default-label-text'>Save this as your default payment method</label>
                         </div>
-                        <button type="submit" className='btn btn-dark'>Add Payment Method</button>
+                        <div className="payment-method-button">
+                            <button type="submit" className='btn payment-method'>
+                                <FontAwesomeIcon icon={faCreditCard} />
+                                {" "} Add Payment Method
+                            </button>
+                        </div>
                         <div className="row">
-                            <div className="col-6"><Link to='/checkout'>Back</Link></div>
-                            <div className="col-6">Secure Connection</div>
+                            <div className="col-6"><Link to='/checkout' className='checkout-link'>Back</Link></div>
+                            <div className="col-6 secure-connection"><i class="bi bi-bag-fill"></i> Secure Connection</div>
                         </div>
                     </form>
                 </div>
