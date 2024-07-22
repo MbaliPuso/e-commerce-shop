b7aeadc3b085b2022bbbd167224eef8aab950111
hexadecimal2 <kidlemon67@gmail.com>
2024-07-22 20:20:47 +0200

    cleaned up ItemView page

--- src/components/Sidebag.jsx
+++ src/components/Sidebag.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import '../styles/Sidebag.css';
+import Card from 'react-bootstrap/Card';
+
+
+
 
 const Sidebag = () => {
     return (
-        <div className='sidebag'>Side Bag</div>
+        <div className='sidebag'>
+            Bag
+
+            <div className='items'>
+
+            <Card>
+                <Card.Body className='card'> <img src="" alt="" /> </Card.Body>
+            </Card>
+
+
+            </div>
+
+            <div className='view-bag'>
+
+            <button type='button'> View Bag </button>
+            </div>
+
+
+        </div>
     );
 };
 
